9c1dc6d2d53b7176124bc296442d5b061f758646
shrihanhegdebca25-blip <shrihanhegdebca25@rvu.edu.in>
2026-02-13 20:05:11 +0530

    added files

---
+++ lab_4 linked list program.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node* next;
+};
+
+int main(){
+    struct node* head = NULL;
+    struct node* second = NULL;
+    struct node* third = NULL;
+    struct node* fourth = NULL;
+
+    head = (struct node*)malloc(sizeof(struct node));
+    second = (struct node*)malloc(sizeof(struct node));
+    third = (struct node*)malloc(sizeof(struct node));
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    fourth = (struct node*)malloc(sizeof(struct node));
+    fourth->data = 4;
+    fourth->next = head;
+
+    struct node* current = fourth;
+    while(current != NULL){
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    
+    return 0;
+}
+
+
